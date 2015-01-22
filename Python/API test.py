43a17a3a0cfa086f342c349c4ea7a9d298d569c2
Byron Coetsee <byroncoetsee@gmail.com>
2015-01-22 13:59:59 +0200

    Added Route screen and a lot of code that goes with it Added SwiftyJSON class to handle JSON data Added CurrentRoute class to handle the currentRoute information

--- Python/API test.py
+++ Python/API test.py
@@ -2,22 +2,22 @@ import urllib2
 import json
 
 
-day = 0
-data = urllib2.urlopen('https://api.forecast.io/forecast/a5e6b097fa6e7b40ee7f51e78512930d/-33.9350,18.5200?units=si')
+# day = 0
+data = urllib2.urlopen('http://54.148.89.48:8080/otp/routers/default/plan?fromPlace=-33.93310591314123%2C18.42128276824951&toPlace=-34.12708173633833%2C18.447933197021484&date=12-20-2014&time=09%3A13+AM&mode=TRANSIT%2CWALK&maxWalkDistance=800&arriveBy=false&wheelchair=false&showIntermediateStops=true')
 dictionary = json.load(data)
 
-print dictionary['daily']['data'][day]
+print dictionary["plan"]["itineraries"][0]["legs"][0]["mode"]
 
-print ''
+# print ''
 
-print dictionary['daily']['data'][day]['summary']
-print dictionary['daily']['data'][day]['temperatureMax']
+# print dictionary['daily']['data'][day]['summary']
+# print dictionary['daily']['data'][day]['temperatureMax']
 
 
 
 # [greeting : "hello", goodbye : "bye"]
 
-["constania" : "22.000,13.0000", "claremont" : "33.0000,43.00000"]
+# ["constania" : "22.000,13.0000", "claremont" : "33.0000,43.00000"]
 
 
 
