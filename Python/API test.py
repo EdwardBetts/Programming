12e9f7cd0b4307a2289a413eb97953a8ee2213e5
Byron Coetsee <byroncoetsee@gmail.com>
2015-01-29 13:18:58 +0200

    Journey now working :D (roughly) ...user can enter their own to/from and click "GoNow"

--- Python/API test.py
+++ Python/API test.py
@@ -6,7 +6,7 @@ import json
 data = urllib2.urlopen('http://54.148.89.48:8080/otp/routers/ZA_11/index/stops')
 dictionary = json.load(data)
 
-print len(dictionary)
+print dictionary[0]
 
 # print dictionary["plan"]["itineraries"][0]["legs"][0]["mode"]
 
